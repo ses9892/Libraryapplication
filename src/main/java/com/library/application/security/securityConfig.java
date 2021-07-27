2a9dadf5f0803ca1f9ab30317707a0ce6366f560
ses9892 <ses1238@gmail.com>
2021-07-27 13:35:43 +0900

    배포

--- src/main/java/com/library/application/security/securityConfig.java
+++ src/main/java/com/library/application/security/securityConfig.java
@@ -51,8 +51,7 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/test","/","/register","/duplication","/email","/forgetPwd").permitAll()
-                .antMatchers("/library/**","/user-service/**","/chat/**","/chating/**").hasRole("USER")
-                .antMatchers("/admin-service/**").hasRole("ADMIN")
+                .antMatchers("/admin-service/**","/chat/room").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                     .formLogin()
